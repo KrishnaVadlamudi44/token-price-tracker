03187a6c5576c0c6e7e283d1617567dc1c304a6a
krishnaVadlamudi44 <krishna4491@outlook.com>
2024-01-20 02:54:29 -0600

    add gecko terminal endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  NetworksData,
+  NetworksResponse,
+  TokenData,
+  TokenResponse,
+} from './types';
+
+const baseUrl = 'https://api.geckoterminal.com/api/v2';
 
 function App() {
+  const fetchNetworks = useCallback(async () => {
+    const networks = await GetNetworks();
+    console.log(networks);
+  }, []);
+
+  const fetchTokenData = useCallback(async () => {
+    const tokensData = GetCoinPrice('test', 'test');
+    console.log(tokensData);
+  }, []);
+
+  useEffect(() => {
+    // fetchNetworks();
+    // fetchTokenData();
+  }, [fetchNetworks, fetchTokenData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
     </div>
   );
 }
+
+const GetNetworks = async (
+  networks: NetworksData[] = [],
+  link?: string
+): Promise<NetworksData[]> => {
+  const res: NetworksResponse = (await (
+    await fetch(link ?? `${baseUrl}/networks`)
+  ).json()) as NetworksResponse;
+
+  networks = networks.concat(...res.data);
+
+  if (res.links.next) {
+    return GetNetworks(networks, res.links.next);
+  }
+
+  return networks;
+};
+
+const GetCoinPrice = async (
+  network: string,
+  addresses: string
+): Promise<TokenData[]> => {
+  const res: TokenResponse = await (
+    await fetch(`${baseUrl}/networks/${network}/tokens/multi/${addresses}`)
+  ).json();
+
+  return res.data;
+};
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,45 @@
+export type NetworksResponse = {
+  data: NetworksData[];
+  links: NetworkLinks;
+};
+
+export type NetworksData = {
+  attributes: NetworkAttributes;
+  id: string;
+  type: string;
+};
+
+export type NetworkAttributes = {
+  name: string;
+  coingecko_asset_platform_id: string;
+};
+
+export type NetworkLinks = {
+  first: string;
+  last: string;
+  next: string;
+  prev: string;
+};
+
+export type TokenResponse = {
+  data: TokenData[];
+};
+
+export type TokenData = {
+  attributes: any;
+  id: string;
+  type: string;
+};
+
+export type TokenAttributes = {
+  address: string;
+  coingecko_coin_id: string;
+  decimals: number;
+  fdv_usd: string;
+  market_cap_usd: string;
+  name: string;
+  price_usd: string;
+  symbol: string;
+  total_reserve_in_usd: string;
+  total_supply: string;
+};
